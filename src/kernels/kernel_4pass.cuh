68bd7151b95199c35ffce4ba00476372db4b2946
Urs Kober <urs.kober@mailbox.tu-dresden.de>
2021-03-31 19:29:21 +0200

    add cub pss

--- src/kernels/kernel_4pass.cuh
+++ src/kernels/kernel_4pass.cuh
@@ -8,7 +8,7 @@ struct iovRow {
     uint32_t mask_repr;
 };
 
-__global__ void kernel_4pass_popcount_monolithic(uint8_t* mask, uint16_t* pss, iovRow* iov, uint32_t chunk_length, uint32_t chunk_count) {
+__global__ void kernel_4pass_popc_monolithic(uint8_t* mask, uint16_t* pss, iovRow* iov, uint32_t chunk_length, uint32_t chunk_count) {
     uint32_t tid = (blockIdx.x * blockDim.x) + threadIdx.x; // thread index
     uint32_t idx = (chunk_length/32) * tid; // index for 1st 32bit-element of this chunk
     if (tid >= chunk_count) {
@@ -23,7 +23,7 @@ __global__ void kernel_4pass_popcount_monolithic(uint8_t* mask, uint16_t* pss, i
     iov[tid] = iovRow{tid, popcount};
 }
 
-__global__ void kernel_4pass_popcount_striding(uint8_t* mask, uint16_t* pss, iovRow* iov, uint32_t chunk_length, uint32_t chunk_count) {
+__global__ void kernel_4pass_popc_striding(uint8_t* mask, uint16_t* pss, iovRow* iov, uint32_t chunk_length, uint32_t chunk_count) {
     for (uint32_t tid = (blockIdx.x * blockDim.x) + threadIdx.x; tid < chunk_count; tid += blockDim.x * gridDim.x) {
         uint32_t idx = (chunk_length/32) * tid; // index for 1st 32bit-element of this chunk
         // assuming chunk_length to be multiple of 32
@@ -33,6 +33,36 @@ __global__ void kernel_4pass_popcount_striding(uint8_t* mask, uint16_t* pss, iov
         }
         pss[tid] = popcount;
         iov[tid] = iovRow{tid, popcount};
+    }
+}
+
+void launch_4pass_popc(uint32_t blockcount, uint32_t threadcount, uint8_t* d_mask, uint16_t* d_pss, iovRow* d_iov, uint32_t chunk_length, uint32_t chunk_count) {
+    if (blockcount == 0) {
+        blockcount = (chunk_count/(chunk_length*threadcount))+1;
+        kernel_4pass_popc_monolithic<<<blockcount, threadcount>>>(d_mask, d_pss, d_iov, chunk_length, chunk_count);
+    } else {
+        kernel_4pass_popc_striding<<<blockcount, threadcount>>>(d_mask, d_pss, d_iov, chunk_length, chunk_count);
+    }
+}
+
+//TODO template by blockdim for static shared memory allocation
+//TODO use shared memory reduction
+__global__ void kernel_4pass_pss_monolithic(uint16_t* pss, uint8_t depth, uint32_t chunk_count, uint32_t* out_count) {
+    uint32_t tid = (blockIdx.x * blockDim.x) + threadIdx.x;
+    tid *= (1<<depth);
+}
+
+__global__ void kernel_4pass_pss_striding(uint16_t* pss, uint8_t depth, uint32_t chunk_count, uint32_t* out_count) {
+    
+}
+
+void launch_4pass_pss(uint32_t blockcount, uint32_t threadcount, uint16_t* d_pss, uint32_t chunk_count, uint32_t* d_out_count) {
+    //TODO repeat for reduction depths
+    if (blockcount == 0) {
+        blockcount = (chunk_count/threadcount)+1;
+        kernel_4pass_pss_monolithic<<<blockcount, threadcount>>>(d_pss, 0, chunk_count, d_out_count);
+    } else {
+        kernel_4pass_pss_striding<<<blockcount, threadcount>>>(d_pss, 0, chunk_count, d_out_count);
     }
 }
 
--- src/main.cu
+++ src/main.cu
@@ -1,4 +1,5 @@
 #include <bitset>
+#include <cub/cub.cuh>
 #include <cstdint>
 #include <iostream>
 #include <stdio.h>
@@ -21,30 +22,44 @@ void gpu_buffer_print(T* d_buffer, uint32_t offset, uint32_t count) {
 
 int main()
 {
-    benchmark_data<uint64_t> bdata(true, 2<<17); // 2<<17 = 1MiB worth of elems (2<<27 = 2GiB)
+    benchmark_data<uint64_t> bdata(true, 128); // 2<<17 = 1MiB worth of elems (2<<27 = 2GiB)
     bdata.generate_mask(MASKTYPE_UNIFORM, 0.5);
 
     
     // 4 pass algo
     uint32_t chunk_length = 32;
-    uint32_t pass1_blockcount = 3;
+    uint32_t pass1_blockcount = 0;
     uint32_t pass1_threadcount = 256;
-    uint32_t pass2_blockcount = 0;
-    uint32_t pass2_threadcount = 256;
+    //uint32_t pass2_blockcount = 0;
+    //uint32_t pass2_threadcount = 256;
 
     uint32_t chunk_count = bdata.count / chunk_length;
-    // #1: pop count per chunk and populate IOV
     uint16_t* d_pss; // prefix sum scan buffer on device
     CUDA_TRY(cudaMalloc(&d_pss, chunk_count*sizeof(uint16_t)));
     iovRow* d_iov; // intermediate optimization vector
     CUDA_TRY(cudaMalloc(&d_iov, chunk_count*sizeof(iovRow)));
-    if (pass1_blockcount == 0) {
-        pass1_blockcount = (chunk_count/(chunk_length*pass1_threadcount))+1;
-        kernel_4pass_popcount_monolithic<<<pass1_blockcount, pass1_threadcount>>>(bdata.d_mask, d_pss, d_iov, chunk_length, chunk_count);
-    } else {
-        kernel_4pass_popcount_striding<<<pass1_blockcount, pass1_threadcount>>>(bdata.d_mask, d_pss, d_iov, chunk_length, chunk_count);
-    }
+    uint32_t* d_pss_total; // pss total
+    CUDA_TRY(cudaMalloc(&d_pss_total, sizeof(uint32_t)));
+    // #1: pop count per chunk and populate IOV
+    launch_4pass_popc(pass1_blockcount, pass1_threadcount, bdata.d_mask, d_pss, d_iov, chunk_length, chunk_count);
     // #2: prefix sum scan (for partial trees)
+    //launch_4pass_pss(pass2_blockcount, pass2_threadcount, d_pss, chunk_count, d_pss_total);
+    {
+        // use cub pss for now
+        uint16_t* d_pss_tmp;
+        CUDA_TRY(cudaMalloc(&d_pss_tmp, chunk_count*sizeof(uint16_t)));
+        void* d_temp_storage = NULL;
+        size_t temp_storage_bytes = 0;
+        CUDA_TRY(cub::DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, d_pss, d_pss_tmp, chunk_count));
+        CUDA_TRY(cudaMalloc(&d_temp_storage, temp_storage_bytes));
+        CUDA_TRY(cub::DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, d_pss, d_pss_tmp, chunk_count));
+        CUDA_TRY(cudaFree(d_temp_storage));
+        //uint16_t* d_pss_die = d_pss;
+        //d_pss = d_pss_tmp;
+        CUDA_TRY(cudaMemcpy(d_pss, d_pss_tmp, chunk_count*sizeof(uint16_t), cudaMemcpyDeviceToDevice));
+        CUDA_TRY(cudaFree(d_pss_tmp));
+    }
+
     gpu_buffer_print(d_pss, 0, 4);
     gpu_buffer_print(reinterpret_cast<uint32_t*>(d_iov), 0, 4);
     // #3: optimization pass (sort or bucket skip launch)
